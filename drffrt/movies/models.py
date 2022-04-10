00af7bcb14b57d3763974e1c018e9a9fba373b4c
shearichard <richard.shea@nec.co.nz>
2022-04-11 00:11:29 +1200

    After 'python manage.py makemigrations movies'

--- drffrt/movies/models.py
+++ drffrt/movies/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Movie(models.Model):
 
-    film = fields.Str(max_length=100) # the title of the film
-    year = fields.SmallIntegerField()
-    audience_score_percent = fields.Decimal(max_digits=4, decimal_places=2, blank=True)
-    genre = fields.Str(max_length=100,allow_blank=True)
-    lead_studio = fields.Str(max_length=100,allow_blank=True)
-    profitability = fields.Decimal(max_digits=10, decimal_places=2, blank=True)
-    rotten_tomatoes_percent = fields.Decimal(max_digits=4, decimal_places=2, blank=True)
-    worldwide_gross_usd = fields.Decimal(max_digits=10, decimal_places=2, blank=True)
+    film = models.CharField(max_length=100) # the title of the film
+    year = models.SmallIntegerField()
+    audience_score_percent = models.DecimalField(max_digits=4, decimal_places=2, blank=True)
+    genre = models.CharField(max_length=100,blank=True)
+    lead_studio = models.CharField(max_length=100,blank=True)
+    profitability = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    rotten_tomatoes_percent = models.DecimalField(max_digits=4, decimal_places=2, blank=True)
+    worldwide_gross_usd = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
 
     class Meta:
-        ordering = ['created']
+        ordering = ['film', 'year']
