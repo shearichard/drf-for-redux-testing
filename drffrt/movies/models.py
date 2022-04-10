6c03757aee149686b8015ca80df09a13fcda0d0d
shearichard <richard.shea@nec.co.nz>
2022-04-11 09:21:22 +1200

    Working to the point where real data is added and may be viewed

--- drffrt/movies/models.py
+++ drffrt/movies/models.py
@@ -4,12 +4,12 @@ class Movie(models.Model):
 
     film = models.CharField(max_length=100) # the title of the film
     year = models.SmallIntegerField()
-    audience_score_percent = models.DecimalField(max_digits=4, decimal_places=2, blank=True)
-    genre = models.CharField(max_length=100,blank=True)
-    lead_studio = models.CharField(max_length=100,blank=True)
-    profitability = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
-    rotten_tomatoes_percent = models.DecimalField(max_digits=4, decimal_places=2, blank=True)
-    worldwide_gross_usd = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    audience_score_percent = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    genre = models.CharField(max_length=100,blank=True, null=True)
+    lead_studio = models.CharField(max_length=100,blank=True, null=True)
+    profitability = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    rotten_tomatoes_percent = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    worldwide_gross_usd = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
 
     class Meta:
         ordering = ['film', 'year']
