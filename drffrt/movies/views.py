23834c72e01db4a5afc4c7276541d4edd3adff78
shearichard <richard.shea@nec.co.nz>
2022-04-11 18:02:38 +1200

    In place of the mixins adopt generic classbased views

--- drffrt/movies/views.py
+++ drffrt/movies/views.py
@@ -1,34 +1,13 @@
-from movies.models import Movie 
+from movies.models import Movie
 from movies.serializers import MovieSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
-class MovieList(  mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+
+class MovieList(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class MovieDetail(  mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class MovieDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
